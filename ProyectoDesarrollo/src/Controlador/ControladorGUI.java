420ed3e7e489364182a27cdad9b2eeafc6d28823
AlivUV <53398158+AlivUV@users.noreply.github.com>
2022-07-24 12:44:21 -0500

    Update ControladorGUI.java

--- ProyectoDesarrollo/src/Controlador/ControladorGUI.java
+++ ProyectoDesarrollo/src/Controlador/ControladorGUI.java
@@ -1,4 +1,4 @@
-/*
+dddddd/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -150,16 +150,6 @@ public class ControladorGUI implements Initializable
             Stage stage = (Stage) source.getScene().getWindow();
             stage.close();
     //    }
-    }
-
-    @FXML
-    private void moverVentana(MouseEvent event)
-    {
-        Node source = (Node) event.getSource();
-        Stage stage = (Stage) source.getScene().getWindow();
-        
-        stage.setX(event.getScreenX() - event.getX() - 8);
-        stage.setY(event.getScreenY());
     }
 
     @FXML
