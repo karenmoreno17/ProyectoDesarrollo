27f5fc92e139668c8a2750ffa288b52f86061e66
ErickAndrade0809 <62568886+ErickAndrade0809@users.noreply.github.com>
2022-07-26 02:14:44 -0500

    Add files via upload
    Se añade el resumen y corrigen errores al insertar en base de datos

--- ProyectoDesarrollo/src/Controlador/ControladorCotizaciones.java
+++ ProyectoDesarrollo/src/Controlador/ControladorCotizaciones.java
@@ -20,6 +20,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
@@ -36,43 +37,8 @@ public class ControladorCotizaciones implements Initializable {
     
     int cantidad_unidades;
     int precio_unidad;
+    int precio_total;
     ObservableList<String> unidadeOp = FXCollections.observableArrayList();
-    @FXML
-    private ComboBox<String> combo_unidades_cotizacion;
-    @FXML
-    private TextField txt_IDautomovil_cotizacion;
-    @FXML
-    private TextField txt_cedula_cotizacion;
-    @FXML
-    private TextField txt_nombre_cotizacion;
-    @FXML
-    private TextField txt_direccion_cotizacion;
-    @FXML
-    private TextField txt_telefono_cotizacion;
-    @FXML
-    private TextField txt_correo_cotizacion;
-    @FXML
-    private TextField txt_genero_cotizacion;
-    @FXML
-    private Button button_buscar_cotizacion;
-    @FXML
-    private TextField txt_IDvendedor_cotizacion;
-    @FXML
-    private TextField txt_marca_cotizacion;
-    @FXML
-    private TextField txt_linea_cotizacion;
-    @FXML
-    private TableView<?> table_resumen_cotizacion;
-    @FXML
-    private Button button_resumen_cotizacion;
-    @FXML
-    private Button button_confirmar_cotizacion;
-    @FXML
-    private TextField txt_modelo_cotizacion;
-    @FXML
-    private TableColumn<?, ?> concepto;
-    @FXML
-    private TableColumn<?, ?> valor;
     @FXML
     private Button bBuscarCliente;
     private ComboBox<String> cbCedula;
@@ -92,13 +58,57 @@ public class ControladorCotizaciones implements Initializable {
     private TextField tfCedula;
     @FXML
     private TextField tfGeneroCliente;
+    @FXML
+    private Label lblUnitario_Resumen;
+    @FXML
+    private Label lblUnidad_Resumen;
+    @FXML
+    private Label lblTotal_Resume;
+    @FXML
+    private Label lblVehiculo_Resumen;
+    @FXML
+    private Button button_buscar_Cliente;
+    @FXML
+    private ComboBox<String> combo_Genero_Cotizacion;
+    @FXML
+    private TextField txt_direccion_cotizacion1;
+    @FXML
+    private ComboBox<String> combo_unidades_cotizacion;
+    @FXML
+    private TextField txt_IDautomovil_cotizacion;
+    @FXML
+    private TextField txt_cedula_cotizacion;
+    @FXML
+    private TextField txt_nombre_cotizacion;
+    @FXML
+    private TextField txt_direccion_cotizacion;
+    @FXML
+    private TextField txt_telefono_cotizacion;
+    @FXML
+    private TextField txt_correo_cotizacion;
+    @FXML
+    private Button button_buscar_cotizacion;
+    @FXML
+    private TextField txt_IDvendedor_cotizacion;
+    @FXML
+    private TextField txt_marca_cotizacion;
+    @FXML
+    private TextField txt_modelo_cotizacion;
+    @FXML
+    private TextField txt_linea_cotizacion;
+    @FXML
+    private Button button_resumen_cotizacion;
+    @FXML
+    private Button button_confirmar_cotizacion;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        combo_Genero_Cotizacion.getItems().add("Masculino");
+        combo_Genero_Cotizacion.getItems().add("Femenino");
+        combo_Genero_Cotizacion.getItems().add("Prefiero no decirlo");
     }    
 
     @FXML
@@ -140,6 +150,12 @@ public class ControladorCotizaciones implements Initializable {
 
     @FXML
     private void generar_resumen(ActionEvent event) {
+        
+        lblVehiculo_Resumen.setText(txt_marca_cotizacion.getText() + " "+txt_linea_cotizacion.getText()+" "+txt_modelo_cotizacion.getText());
+        lblUnidad_Resumen.setText(precio_unidad+"");
+        precio_total = precio_unidad * Integer.parseInt((combo_unidades_cotizacion.getValue()));
+        lblTotal_Resume.setText(precio_total+"");
+        
     }
 
     @FXML
@@ -150,17 +166,19 @@ public class ControladorCotizaciones implements Initializable {
         Connection conexion = con.getConnection();
         
         try {
-            
+                   
             String sql_cotizacion="INSERT INTO cotizacion (id_vehiculo, id_cliente,id_vendedor) VALUES (" 
                     + Integer.parseInt(txt_IDautomovil_cotizacion.getText())+","+Integer.parseInt(txt_cedula_cotizacion.getText())+","+Integer.parseInt(txt_IDvendedor_cotizacion.getText())+");";
             Statement stCotizacion= conexion.createStatement();
-            ResultSet rstCotizacion = stCotizacion.executeQuery(sql_cotizacion);
-            String sql_cliente="INSERT INTO cliente (cedula_cliente, nombre_cliente,direccion_cliente,telefono_cliente,genero_cliente,correo_electronico) VALUES ("
-                    + Integer.parseInt(txt_cedula_cotizacion.getText())+",'"+txt_nombre_cotizacion.getText()+"','"+txt_direccion_cotizacion.getText()+"',"+Integer.parseInt(txt_telefono_cotizacion.getText())+",'"+txt_genero_cotizacion.getText()+"','"+txt_correo_cotizacion.getText()+"');";
-            System.out.println(sql_cliente);
+            int cotizacion_insertar=stCotizacion.executeUpdate(sql_cotizacion);
             
+            String sql_cliente="INSERT INTO cliente (cedula_cliente, nombre_cliente,direccion_cliente,telefono_cliente,genero_cliente,correo_electronico) VALUES ("
+                    + Integer.parseInt(txt_cedula_cotizacion.getText())+",'"+txt_nombre_cotizacion.getText()+"','"+txt_direccion_cotizacion.getText()+"',"+Integer.parseInt(txt_telefono_cotizacion.getText())+",'"+combo_Genero_Cotizacion.getValue()+"','"+txt_correo_cotizacion.getText()+"');";              
             Statement stCliente= conexion.createStatement();
-            ResultSet rstCliente = stCliente.executeQuery(sql_cliente);
+            stCliente.executeUpdate(sql_cliente);
+            
+            
+            
             conexion.close();  
             
         } catch (SQLException ex) {
@@ -314,6 +332,41 @@ public class ControladorCotizaciones implements Initializable {
             };
             temporizador.schedule(tarea, 0);
         }
+    }
+   
+
+    @FXML
+    private void buscar_cliente(ActionEvent event) {
+        
+       if(!(txt_cedula_cotizacion.getText().isEmpty())){
+            
+            Fachada con = new Fachada();
+            Connection conexion = con.getConnection();
+            
+            try {
+            String sql_buscar_automovil = "SELECT * FROM cliente WHERE cedula_cliente = " + txt_cedula_cotizacion.getText()+";";                                 
+            Statement sentencia_buscar_cliente = conexion.createStatement();
+            ResultSet rst_buscar_cliente =sentencia_buscar_cliente.executeQuery(sql_buscar_automovil);
+            rst_buscar_cliente.next();
+            txt_nombre_cotizacion.setText(rst_buscar_cliente.getString(2));
+            txt_direccion_cotizacion.setText(rst_buscar_cliente.getString(3));
+            txt_telefono_cotizacion.setText(rst_buscar_cliente.getString(4));
+   
+            txt_correo_cotizacion.setText(rst_buscar_cliente.getString(6));
+            
+            combo_Genero_Cotizacion.setValue(rst_buscar_cliente.getString(5));
+                                  
+             conexion.close();       
+        } catch (SQLException ex) {
+            Logger.getLogger(ControladorCotizaciones.class.getName()).log(Level.SEVERE, null, ex);
+        }
+            
+        }else{
+            
+            
+            
+        } 
+        
     }
 
 
