bb84dba8039aada232438720b537108d922eb4be
ErickAndrade0809 <62568886+ErickAndrade0809@users.noreply.github.com>
2022-07-27 12:05:21 -0500

    Add files via upload
    Añade Modelo de sedes

---
+++ ProyectoDesarrollo/src/Modelo/Sede.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import Controlador.ControladorCotizaciones;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+/**
+ *
+ * @author dinos
+ */
+public class Sede {
+    private int ID;
+    private String nombre;
+    private String ubicacion;
+    private int telefono;
+
+    public Sede(int ID, String nombre, String ubicacion, int telefono) {
+        this.ID = ID;
+        this.nombre = nombre;
+        this.ubicacion = ubicacion;
+        this.telefono = telefono;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    
+}
+    
+    
